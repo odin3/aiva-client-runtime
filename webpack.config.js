0eef2bbd307d992df77a314e6b84a48c3603cd70
odin3 <ascii@live.ru>
2017-04-07 04:38:37 +0300

    Fix build config

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,12 @@ module.exports = {
         filename: './dist/script.js'
     },
     resolve: {
-        root: [
-            path.resolve('./src/modules'),
-            path.resolve('node_modules')
-        ],
-        extensions: ['', '.ts', '.js']
+        extensions: ['.ts', '.js']
     },
     module: {
         loaders: [{
             test: /\.tsx?$/,
-            loader: 'ts-loader!preprocessor?file&config=preprocess-ts.json'
+            loader: 'ts-loader'
         }]
     }
 };
